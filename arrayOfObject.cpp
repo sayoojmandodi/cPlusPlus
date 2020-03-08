bdfe6b0e19c3b77cb2c10682e477451d53fc2fbc
Sayooj Mandodi <sayooj.m@zagaz.in>
2020-03-08 18:13:17 +0530

    move operation mentioned

--- arrayOfObject.cpp
+++ arrayOfObject.cpp
@@ -14,25 +14,37 @@ public:
           std::cout << "param const called ..." << std::endl;
      }
 
-     Cbaseclass(Cbaseclass &&obj)
+     Cbaseclass(Cbaseclass &&obj) noexcept
      {
-          std::cout << "move const get called" << std::endl;
+          std::cout << "mov get called" << std::endl;
           x = obj.x;
           this->ptr = obj.ptr;
           obj.ptr = nullptr;
      }
 
-     ~Cbaseclass() { delete ptr; }
+     ~Cbaseclass()
+     {
+          delete ptr;
+     }
 };
 
 int main()
 {
      std::vector<Cbaseclass> a;
      a.reserve(100);
+     Cbaseclass c(10);
+     Cbaseclass d(std::move(c)); // Since copy constr is not declared we need to explicity mention it should call \
+     //                               move constructor or else it will not compile. if we declare a move constructor \
+     //                               compiler will not declare a copy constructor by default.
+
      for (size_t i = 0; i < 10; ++i)
      {
           a.push_back(Cbaseclass(1)); // give it arguments of type needed for construction
      }
+
+     // int *testPtr = new int;
+     // delete testPtr;
+     // delete testPtr;
 
      return 0;
 }
